c194a97a6c10df7d9963b0d9b65b26619e8cdc1e
KranthiAdabala <87488350+KranthikillerOp@users.noreply.github.com>
2023-08-12 21:13:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 // This function is called when the form is submitted.
 function submitForm() {
-    // Get the form data.
-    var name = document.getElementById("name").value;
-    var phone_number = document.getElementById("phone_number").value;
-    var address = document.getElementById("address").value;
-    var have_lived_with_dog = document.getElementById("have_lived_with_dog").checked;
-    var preferred_breeds = document.getElementById("preferred_breeds").options;
-    var about_you = document.getElementById("about_you").value;
+  // Get the form data.
+  var name = document.getElementById("name").value;
+  var phone = document.getElementById("phone").value;
+  var address = document.getElementById("address").value;
+  var have_lived_with_dog = document.getElementById("have_lived_with_dog").checked;
+  var preferred_breeds = document.getElementById("preferred_breeds").value;
 
-    // Send the form data to the server.
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/dog_adoption_application", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify({
-        name: name,
-        phone_number: phone_number,
-        address: address,
-        have_lived_with_dog: have_lived_with_dog,
-        preferred_breeds: preferred_breeds.map(function(option) { return option.value; }),
-        about_you: about_you
-    }));
+  // Send the form data to the server.
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "/submit");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(JSON.stringify({
+    name: name,
+    phone: phone,
+    address: address,
+    have_lived_with_dog: have_lived_with_dog,
+    preferred_breeds: preferred_breeds
+  }));
 
-    // When the response is received, show a success message.
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            alert("Your application has been submitted successfully.");
-        } else {
-            alert("An error occurred while submitting your application.");
-        }
-    };
+  // When the server responds, show a success message.
+  xhr.onload = function() {
+    if (xhr.status == 200) {
+      alert("Your application has been submitted successfully!");
+    } else {
+      alert("There was an error submitting your application.");
+    }
+  };
 }
 
-// Add an event listener to the submit button.
-document.getElementById("submit").addEventListener("click", submitForm);
+// When the submit button is clicked, call the submitForm() function.
+document.getElementById("submit").onclick = submitForm;;
